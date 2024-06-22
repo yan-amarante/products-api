0a529c48d0d486e00e723cb7d85916db12948a73
Yan Amarante <yanlimaamarantec@hotmail.com>
2024-06-22 13:18:11 -0300

    first commit

---
+++ springboot/src/main/java/com/example/springboot/models/ProductModel.java
@@ -0,0 +1,43 @@
+package com.example.springboot.models;
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.UUID;
+import org.springframework.hateoas.RepresentationModel;
+
+@Entity
+@Table(name = "TB PRODUCTS")
+public class ProductModel extends RepresentationModel<ProductModel> implements Serializable {
+    private static final long serialVersionId = 1;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID idProduct;
+    private String name;
+    private BigDecimal value;
+
+    public UUID getIdProduct() {
+        return idProduct;
+    }
+
+    public void setIdProduct(UUID idProduct) {
+        this.idProduct = idProduct;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public BigDecimal getValue() {
+        return value;
+    }
+
+    public void setValue(BigDecimal value) {
+        this.value = value;
+    }
+}
